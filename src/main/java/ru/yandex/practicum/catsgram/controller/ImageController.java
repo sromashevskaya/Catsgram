a45720eaa19308bea7a1db54a44d06a3635af2e0
svetlana.romashevskaya <lana.romashevskaya@gmail.com>
2024-11-27 17:03:44 +0300

    Добавлен ImageController

--- src/main/java/ru/yandex/practicum/catsgram/controller/ImageController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ImageController.java
@@ -5,6 +5,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.yandex.practicum.catsgram.model.Image;
+import ru.yandex.practicum.catsgram.model.ImageData;
 import ru.yandex.practicum.catsgram.service.ImageService;
 
 import java.util.List;
@@ -24,5 +25,18 @@ public class ImageController {
     public List<Image> addPostImages(@PathVariable("postId") long postId,
                                      @RequestParam("image") List<MultipartFile> files) {
         return imageService.saveImages(postId, files);
+    }
+
+    @GetMapping(value = "/images/{imageId}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<byte[]> downloadImage(@PathVariable long imageId) {
+        ImageData imageData = imageService.getImageData(imageId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentDisposition(
+                ContentDisposition.attachment()
+                        .filename(imageData.getName())
+                        .build()
+        );
+
+        return new ResponseEntity<>(imageData.getData(), headers, HttpStatus.OK);
     }
 }
