775cfe12c1e343d102085d8ba7dcbc8e03808eaf
svetlana.romashevskaya <lana.romashevskaya@gmail.com>
2024-12-18 00:53:57 +0300

    Добавлены изменения

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ErrorHandler.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.exception.ParameterNotValidException;
+
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleIncorrectParameterException(final ParameterNotValidException e) {
+        return new ErrorResponse(
+                String.format("Некорректное значение параметра %s: %s", e.getParameter(), e.getReason())
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErrorResponse handleConditionsNotMetException(final ConditionsNotMetException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleDuplicatedDataException(final DuplicatedDataException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable ignored) {
+        return new ErrorResponse(
+                "Произошла непредвиденная ошибка."
+        );
+    }
+}
+
+
--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.catsgram.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ParameterNotValidException;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
@@ -17,18 +18,28 @@ public class PostController {
         this.postService = postService;
     }
 
-    /*  @GetMapping
-    public Collection<Post> findAll() {
-        return postService.findAll();
-    } */
-
     @GetMapping
     public Collection<Post> findAll(
             @RequestParam(value = "from", defaultValue = "0", required = false) Integer from,
             @RequestParam(value = "size", defaultValue = "10", required = false) Integer size,
             @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort
     ) {
-        // Возвращаем посты с учетом параметров или по умолчанию
+        // Проверка параметра sort
+        if (!"asc".equalsIgnoreCase(sort) && !"desc".equalsIgnoreCase(sort)) {
+            throw new ParameterNotValidException("sort", "Некорректное значение сортировки. Допустимы только 'asc' или 'desc'.");
+        }
+
+        // Проверка параметра size
+        if (size <= 0) {
+            throw new ParameterNotValidException("size", "Некорректный размер выборки. Размер должен быть больше нуля.");
+        }
+
+        // Проверка параметра from
+        if (from < 0) {
+            throw new ParameterNotValidException("from", "Некорректное значение начального индекса. Индекс не может быть меньше нуля.");
+        }
+
+        // Возвращаем посты с учетом параметров
         return postService.findAll(from, size, sort);
     }
 
